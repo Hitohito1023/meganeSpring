00ec06c6e2f2967d37a7d1bb9662138094bd8659
tanno.masayoshi <tanno.masayoshi@m.alhinc.jp>
2021-08-04 11:05:31 +0900

    20210803 11:05 getAllメソッド追加

--- src/main/java/com/megane/controller/UserController.java
+++ src/main/java/com/megane/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.megane.controller;
 
-public class UserController {
+import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.megane.entity.User;
+import com.megane.repository.UserRepository;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	UserRepository userRepository;
+
+	@CrossOrigin
+	@GetMapping("get-all")
+	public List<User> getAllUser() throws IOException {
+		return userRepository.findAll();
+	}
 }
--- src/main/java/com/megane/entity/User.java
+++ src/main/java/com/megane/entity/User.java
@@ -1,5 +1,49 @@
 package com.megane.entity;
 
-public class User {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@JsonIgnoreProperties(ignoreUnknown=true)
+@Getter
+@Setter
+public class User {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "account")
+	private String account;
+
+	@Column(name= "admin_flg")
+	private Boolean adminFlg;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "team")
+	private Integer team;
+
+	@Column(name = "suspended")
+	private Boolean suspended;
+
+	@Column(name = "created_date")
+	private Date created_date;
 }
