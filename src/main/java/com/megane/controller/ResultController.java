9b928261c5e2f0a2ed45aefb44c142e89e514325
[ryota1023-sleep] <[kinase.ryota@m.alhinc.jp]>
2021-08-04 10:41:27 +0900

    [finish]entity作成

--- src/main/java/com/megane/controller/ResultController.java
+++ src/main/java/com/megane/controller/ResultController.java
@@ -1,5 +1,18 @@
 package com.megane.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.megane.service.ResultService;
+
+@RestController
+@RequestMapping("result")
 public class ResultController {
+
+	@Autowired
+	ResultService resultService;
+
+
 
 }
