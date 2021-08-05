274c4e6adf64e7896f77221ccd7f315ee6720b5f
tanno.masayoshi <tanno.masayoshi@m.alhinc.jp>
2021-08-06 00:17:51 +0900

    20210806 0:17 UserController修正

--- src/main/java/com/megane/repository/UserRepository.java
+++ src/main/java/com/megane/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.megane.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +7,5 @@ import com.megane.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-	Optional<User> findById(Integer userId);
-
 	User findByAccountAndPassword(String account, String password);
-
 }
